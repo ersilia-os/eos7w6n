7f8c1e68d23f247cafb1fac59414c2d87a3aa9d5
emmakodes <emmamichael65@gmail.com>
2023-07-11 05:23:12 -0700

    refactor code

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,9 +1,9 @@
-python grover/scripts/save_features.py --data_path data.csv \
+python code/scripts/save_features.py --data_path data.csv \
                                        --save_path features.npz \
                                        --features_generator rdkit_2d_normalized \
                                        --restart
 
-python grover/main.py fingerprint --data_path data.csv \
+python code/main.py fingerprint --data_path data.csv \
                                   --features_path features.npz \
                                   --checkpoint_path ../checkpoints/grover_large.pt \
                                   --fingerprint_source both \
